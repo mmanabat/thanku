42187f9f0fe8d977cfb853a06b09c7e5f6587baf
bnicart <bnicart@you-source.com>
2015-01-29 01:35:14 +0800

    [Created newsfeed(realtime data). Beat it! xD]

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 var mainApp = angular.module("thanku", []);
 
-mainApp
-
 mainApp.controller("LoginController", ['$scope', function($scope) {
 
 }]);
@@ -25,8 +23,34 @@ mainApp.controller("UserListController", ['$scope', function($scope) {
     };
 }]);
 
-mainApp.controller("NewsFeedController", ['$scope', function($scope) {
+mainApp.controller("NewsFeedController", ['$scope', '$http', function($scope, $http) {
 
+    var baseUrl = "http://13d6f749.ngrok.com";
+
+    $scope.init = function() {
+        $scope.getNewsFeed();
+    };
+
+    $scope.getNewsFeed = function() {
+
+        var promise = $http({
+            method: "GET",
+            async: true,
+            url: baseUrl + "/api/v1.0/credits?time="+ Date.now().toString()
+        });
+
+        promise.success( function(data) {
+            $scope.newsFeed = data.credits;
+        }).error( function(data) {
+            toastr.error(data);
+        });
+    };
+
+    $scope.getMomentTime = function(timestamp) {
+        return (moment(timestamp).fromNow());
+    };
+
+    setInterval(function(){ $scope.getNewsFeed(); }, 3000);
 }]);
 
 mainApp.controller("TopThreeController", ['$scope', function($scope) {
